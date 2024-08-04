742c20b54743cdeedf1c7ed70b8ae2d546ac3774
槿南11 <52593036+jinnan11@users.noreply.github.com>
2024-08-04 14:59:43 +0800

    Update cf-f.sh

--- cf-f.sh
+++ cf-f.sh
@@ -1,4 +1,5 @@
 wget https://raw.githubusercontent.com/jinnan11/serv00_api/main/cloudflared-freebsd-2024.6.1.7z && 7z x cloudflared-freebsd-2024.6.1.7z && rm cloudflared-freebsd-2024.6.1.7z && mv -f ./temp/* ./cloudflared && rm -rf temp
 
+clear
 echo "作者：槿南 https://i.jnpan.top"
 echo
