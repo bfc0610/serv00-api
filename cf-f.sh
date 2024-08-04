b1089b6efca5fb74f53ae98d1569414f31aeaa37
槿南11 <52593036+jinnan11@users.noreply.github.com>
2024-08-04 14:25:46 +0800

    Add files via upload

---
+++ cf-f.sh
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/jinnan11/serv00_api/main/cloudflared-freebsd-2024.6.1.7z && 7z x cloudflared-freebsd-2024.6.1.7z && rm cloudflared-freebsd-2024.6.1.7z && mv -f ./temp/* ./cloudflared && rm -rf temp
