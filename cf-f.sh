117c68cee9abcd23acf0d055547d0c4c12b5fc71
槿南11 <52593036+jinnan11@users.noreply.github.com>
2024-08-07 23:23:01 +0800

    Update cf-f.sh

--- cf-f.sh
+++ cf-f.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/jinnan11/serv00_api/main/cloudflared-freebsd-2024.6.1.7z && 7z x cloudflared-freebsd-2024.6.1.7z && rm cloudflared-freebsd-2024.6.1.7z && mv -f ./temp/* ./cloudflared && rm -rf temp
+wget https://raw.githubusercontent.com/jinnan11/serv00_api/main/cloudflared-freebsd-2024.8.2.7z && 7z x cloudflared-freebsd-2024.8.2.7z && rm cloudflared-freebsd-2024.8.2.7z && mv -f ./temp/* ./cloudflared && rm -rf temp
 
 echo 
 echo "cloudflared-freebsd下载完成！"
